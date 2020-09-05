e90392fb65a2782aac0cc25ce97b926bf0507ea0
jazz dhami <me@jazzdhami.com>
2020-09-05 09:53:07 +0530

    Updates

---
+++ precache-manifest.5279e5894bd4a944006f03cc890da753.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57fd3164c47407dfe18657131e7a5513",
+    "url": "/reactlivewebsite/index.html"
+  },
+  {
+    "revision": "d6bc51834a5e1cc6f726",
+    "url": "/reactlivewebsite/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "f268fb7c288654b347ef",
+    "url": "/reactlivewebsite/static/css/main.3b5229f7.chunk.css"
+  },
+  {
+    "revision": "d6bc51834a5e1cc6f726",
+    "url": "/reactlivewebsite/static/js/2.308ab806.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactlivewebsite/static/js/2.308ab806.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f268fb7c288654b347ef",
+    "url": "/reactlivewebsite/static/js/main.09d4cb78.chunk.js"
+  },
+  {
+    "revision": "42556da885f19fc00f91",
+    "url": "/reactlivewebsite/static/js/runtime-main.884338d5.js"
+  },
+  {
+    "revision": "92a0deec07164fd01177670fc3e592ad",
+    "url": "/reactlivewebsite/static/media/img11.92a0deec.jfif"
+  },
+  {
+    "revision": "5b3e04b28b1c4335159ad67db3679ed5",
+    "url": "/reactlivewebsite/static/media/img12.5b3e04b2.jfif"
+  },
+  {
+    "revision": "639112cc1d60f2113c9ceca882f43592",
+    "url": "/reactlivewebsite/static/media/img13.639112cc.jfif"
+  },
+  {
+    "revision": "0b43915d82c2ab7783ed2a4ab8ed079b",
+    "url": "/reactlivewebsite/static/media/img14.0b43915d.jfif"
+  },
+  {
+    "revision": "8f1174957ad9cc87484735eaf0c7da21",
+    "url": "/reactlivewebsite/static/media/img15.8f117495.jfif"
+  },
+  {
+    "revision": "fba7768ccb31f34c227bee1b5206be33",
+    "url": "/reactlivewebsite/static/media/img16.fba7768c.jfif"
+  },
+  {
+    "revision": "70bb8b761fadd1146dc3c03f21cea37c",
+    "url": "/reactlivewebsite/static/media/img17.70bb8b76.jfif"
+  },
+  {
+    "revision": "2e0dbde8ee89765480d9ef77ca47634f",
+    "url": "/reactlivewebsite/static/media/img19.2e0dbde8.jfif"
+  },
+  {
+    "revision": "7f07d2610eea97dd23da1b21a7dbdd52",
+    "url": "/reactlivewebsite/static/media/img2.7f07d261.jfif"
+  }
+]);
